b45e1d2cc12fd58cb7e6938b749a522c178c3792
Hamptonzhou <Hamptonzhou@163.com>
2019-01-24 09:16:48 +0800

    对vue-cli 3的配置文件注释说明

--- vue.config.js
+++ vue.config.js
@@ -12,34 +12,34 @@ const resolve = dir => {
 // 例如：https://www.foobar.com/my-app/
 // 需要将它改为'/my-app/'
 // iview-admin线上演示打包路径： https://file.iviewui.com/admin-dist/
-const BASE_URL = process.env.NODE_ENV === 'production'
-  ? '/'
-  : '/'
+const BASE_URL = process.env.NODE_ENV === 'production' ?
+  '/' :
+  '/'
 
+module.exports = {
+  // 打包文件输出文件目录
+  // outputDir: 'dist'
 
   //  "baseUrl" option in vue.config.js is deprecated now, please use "publicPath" instead.
-module.exports = {
-  // Project deployment base
-  // By default we assume your app will be deployed at the root of a domain,
-  // e.g. https://www.my-app.com/
-  // If your app is deployed at a sub-path, you will need to specify that
-  // sub-path here. For example, if your app is deployed at
-  // https://www.foobar.com/my-app/
-  // then change this to '/my-app/'
   baseUrl: BASE_URL,
-  // tweak internal webpack configuration.
-  // see https://github.com/vuejs/vue-cli/blob/dev/docs/webpack.md
-  // 如果你不需要使用eslint，把lintOnSave设为false即可
+
+  // 是否在保存时使用eslint-loader进行代码规范检查
   lintOnSave: false,
+
+  // 生产环境是否生成 sourceMap 文件
+  productionSourceMap: false,
+
+  // webpack配置。
   chainWebpack: config => {
     config.resolve.alias
-      .set('@', resolve('src')) // key,value自行定义，比如.set('@@', resolve('src/components'))
+      .set('@', resolve('src')) // 为src目录设置别名='@'
       .set('_c', resolve('src/components'))
   },
-  // 设为false打包时不生成.map文件
-  productionSourceMap: false
-  // 这里写你调用接口的基础路径，来解决跨域，如果设置了代理，那你本地开发环境的axios的baseUrl要写为 '' ，即空字符串
-  // devServer: {
-  //   proxy: 'localhost:3000'
-  // }
+
+  // webpack-dev-server 相关配置
+  devServer: {
+    // host: '0.0.0.0',
+    port: 9601,
+  }
+
 }
