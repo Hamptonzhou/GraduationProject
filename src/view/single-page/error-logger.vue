498405ebb586f82b2d3b7f498bc38e6dfca4f3ad
Hamptonzhou <Hamptonzhou@163.com>
2019-01-24 09:19:51 +0800

    初始化日志页面

--- src/view/single-page/error-logger.vue
+++ src/view/single-page/error-logger.vue
@@ -1,15 +1,33 @@
 <template>
   <div>
-    <Button @click="exportData" type="primary" style="margin: 0 10px 10px 0;">导出日志记录</Button>
-    <Table ref="table" :columns="columns" :data="errorList"></Table>
+    <div class="form-inline">
+      <Button @click="exportData" type="primary" style="margin: 0 10px 10px 0;">导出日志记录</Button>
+      <DatePicker
+        type="daterange"
+        split-panels
+        placeholder="筛选日期"
+        style="width: 200px"
+        @on-change="handleChange"
+      ></DatePicker>
+    </div>
+    <Table border stripe @on-row-click="getRowClick" ref="table" :columns="columns" :data="logList"></Table>
+    <Page
+      :total="total"
+      show-elevator
+      show-sizer
+      show-total
+      @on-change="getCurrentPage"
+      @on-page-size-change="getPageSize"
+    />
   </div>
 </template>
 
 <script>
-import dayjs from "dayjs";
-import { mapMutations } from "vuex";
+import logCollectionApi from "@/api/LogCollection.js";
+import axios from "axios";
+
 export default {
-  name: 'error_logger_page',
+  name: "error_logger_page",
   data() {
     return {
       columns: [
@@ -62,55 +80,70 @@ export default {
           align: "center",
           width: 150
         }
-      ]
+      ],
+      logList: [],
+      total: null,
+      currentPage: 1,
+      pageSize: 20,
+      startDate: null,
+      endDate: null
     };
   },
-  computed: {
-    errorList() {
-      return this.$store.state.app.errorList;
-    }
+  mounted() {
+    this.getLogList();
   },
+
   methods: {
-    ...mapMutations(["setHasReadErrorLoggerStatus"]),
-    exportData() {
-      this.$refs.table.exportCsv({
-        filename: "错误日志.csv"
-      });
-    },
-    fetchData() {
-      operationLogApi
-        .getOperationLogList(
-          this.currentPage,
-          this.pageSize,
-          this.startDate,
-          this.endDate,
-          this.keyword
-        )
-        .then(res => {
-          if (res.status === 0) {
-            this.tableData = res.data.rows;
-            this.total = res.data.total;
-          } else {
-            notification({
-              type: "warning",
-              title: res.message || "获取操作日志有误"
-            });
+    getLogList: function() {
+      axios
+        .get("http://localhost:9600/LogAnalyseController/getOperationLogList", {
+          params: {
+            page: this.currentPage,
+            rows: this.pageSize,
+            startDate: this.startDate,
+            endDate: this.endDate
           }
         })
-        .catch(err => {
-          this.isLoading = false;
-          notification({
-            type: "error",
-            title: err.message || "获取操作日志有误"
-          });
+        .then(res => {
+          if (res.data.status === 0) {
+            this.logList = res.data.data.rows;
+            this.total = res.data.data.total;
+          }
         });
+    },
+    getRowClick(rowData) {
+      alert(rowData);
+    },
+
+    //日期选择框变化时，触发@on-change后调用该函数，进行日期设置
+    handleChange(date) {
+      this.startDate = date[0];
+      this.endDate = date[1];
+      console.log(this.startDate);
+      console.log(this.endDate);
+    },
+    //
+    getCurrentPage(currentPage) {
+      this.currentPage = currentPage;
+      console.log(this.currentPage);
+      if (this.logList !== null) {
+        this.getLogList();
+      }
+    },
+    getPageSize(pageSize) {
+      this.pageSize = pageSize;
+      console.log(this.pageSize);
+      if (this.logList !== null) {
+        this.getLogList();
+      }
+    },
+
+    //导出csv文件
+    exportData() {
+      this.$refs.table.exportCsv({
+        filename: new Date() + "错误日志.csv"
+      });
     }
-  },
-  activated() {
-    this.setHasReadErrorLoggerStatus();
-  },
-  mounted() {
-    this.setHasReadErrorLoggerStatus();
   }
 };
 </script>
