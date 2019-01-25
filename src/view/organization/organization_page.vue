76605b9e1733901381b104ca98e2ce3be462fa3b
Hamptonzhou <Hamptonzhou@163.com>
2019-01-25 20:25:28 +0800

    组织模块初始化页面

--- src/view/organization/organization_page.vue
+++ src/view/organization/organization_page.vue
@@ -1,10 +1,20 @@
 <template>
-  <div></div>
+  <div class="panel-toolbar">
+    <Button type="primary">新建部门</Button>
+    <Divider type="vertical"/>
+    <Button type="primary">新建人员</Button>
+    <Divider type="horizontal"/>
+    <add-people></add-people>
+  </div>
 </template>
 
 <script>
+import AddPeople from "@/view/organization/AddPeople.vue";
 export default {
   name: "organization_page",
+  components: {
+    AddPeople
+  },
   data() {
     return {};
   },
