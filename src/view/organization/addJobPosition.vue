2da7e00ee8d5036832059783ee7c0055e7f487da
Hamptonzhou <Hamptonzhou@163.com>
2019-01-26 17:12:44 +0800

    初始化新建部门、新建岗位页面

---
+++ src/view/organization/addJobPosition.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>这是新建岗位页面</h1>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {},
+  mounted() {}
+};
+</script>
+<style>
+</style>
--- src/view/organization/organization_page.vue
+++ src/view/organization/organization_page.vue
@@ -2,18 +2,27 @@
   <div class="panel-toolbar">
     <Button type="primary">新建部门</Button>
     <Divider type="vertical"/>
+    <Button type="primary">新建岗位</Button>
+    <Divider type="vertical"/>
     <Button type="primary">新建人员</Button>
     <Divider type="horizontal"/>
     <add-people></add-people>
+    <add-job-position></add-job-position>
+    <add-department></add-department>
   </div>
 </template>
 
 <script>
-import AddPeople from "@/view/organization/AddPeople.vue";
+import addPeople from "@/view/organization/addPeople.vue";
+import addJobPosition from "@/view/organization/addJobPosition.vue";
+import addDepartment from "@/view/organization/addDepartment.vue";
+
 export default {
   name: "organization_page",
   components: {
-    AddPeople
+    addPeople,
+    addJobPosition,
+    addDepartment
   },
   data() {
     return {};
