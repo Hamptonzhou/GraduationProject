88456a5fb02841498ac64f452bdd7c84ab49b698
Hamptonzhou <Hamptonzhou@163.com>
2019-01-31 20:40:12 +0800

    完善工作流模块

--- src/view/workflowDesigne/components/createProcess.vue
+++ src/view/workflowDesigne/components/createProcess.vue
@@ -1,24 +1,35 @@
 <template>
   <div>
-    <h1>新建流程显示页面</h1>
-    <button @click="getPage">调出页面</button>
+    <Button type="primary" size="large" @click="getWorkflowDesignPage">调出流程图设计页面</Button>
   </div>
 </template>
 
 <script>
 import workflowDesignApi from "@/api/workflowDesign.js";
 export default {
-  name: "",
   data() {
     return {};
   },
   methods: {
-    getPage() {
-      workflowDesignApi.showWorkflowDesignPage();
-    }
-  },
-  mounted() {
     //发出请求画图页面的请求
+    getWorkflowDesignPage() {
+      workflowDesignApi
+        .showWorkflowDesignPage()
+        .then(res => {
+          if (res.status === 0) {
+            console.log(res.data.modelId, res.data.redirect);
+            //打开新窗口
+            window.open(
+              "http://localhost:9600/modeler.html?modelId=" + res.data.modelId
+            );
+          } else {
+            console.log("else---程序出错状态码非零");
+          }
+        })
+        .catch(err => {
+          console.log("catch---请求服务器错误");
+        });
+    }
   }
 };
 </script>
