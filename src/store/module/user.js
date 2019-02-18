1ecd63dcabf404d0941df183f47e1daeae23c992
Hamptonzhou <Hamptonzhou@163.com>
2019-02-19 00:14:11 +0800

    修改代码格式

--- src/store/module/user.js
+++ src/store/module/user.js
@@ -199,7 +199,7 @@ export default {
         })
       })
     },
-        // 登录
+    // 登录
     handleLogin ({ commit }, {userName, password}) {
       userName = userName.trim()
       return new Promise((resolve, reject) => {
@@ -208,6 +208,7 @@ export default {
           userName,
           password
         }).then(res => {
+          console.log(res)
           const data = res.data
           commit('setToken', data.token)
           resolve()
@@ -216,27 +217,12 @@ export default {
         })
       })
     },
-    // 退出登录
-    handleLogOut ({ state, commit }) {
-      return new Promise((resolve, reject) => {
-        logout(state.token).then(() => {
-          commit('setToken', '')
-          commit('setAccess', [])
-          resolve()
-        }).catch(err => {
-          reject(err)
-        })
-        // 如果你的退出登录无需请求接口，则可以直接使用下面三行代码而无需使用logout调用接口
-        // commit('setToken', '')
-        // commit('setAccess', [])
-        // resolve()
-      })
-    },
     // 获取用户相关信息
     getUserInfo ({ state, commit }) {
       return new Promise((resolve, reject) => {
         try {
           getUserInfo(state.token).then(res => {
+            console.log(res)
             const data = res.data
             commit('setAvator', data.avator)
             commit('setUserName', data.name)
@@ -250,6 +236,22 @@ export default {
         } catch (error) {
           reject(error)
         }
+      })
+    },
+     // 退出登录
+     handleLogOut ({ state, commit }) {
+      return new Promise((resolve, reject) => {
+        logout(state.token).then(() => {
+          commit('setToken', '')
+          commit('setAccess', [])
+          resolve()
+        }).catch(err => {
+          reject(err)
+        })
+        // 如果你的退出登录无需请求接口，则可以直接使用下面三行代码而无需使用logout调用接口
+        // commit('setToken', '')
+        // commit('setAccess', [])
+        // resolve()
       })
     },
   }
