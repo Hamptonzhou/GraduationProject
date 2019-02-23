05b322760898e93f2435671a6c41615e1867eb58
Hamptonzhou <Hamptonzhou@163.com>
2019-02-23 23:49:04 +0800

    初始化在办工作页面

--- src/view/myWork/hanglingWork.vue
+++ src/view/myWork/hanglingWork.vue
@@ -1,16 +1,170 @@
 <template>
   <div>
-    <h1>在办工作页面</h1>
+    <Form inline>
+      <FormItem>
+        <Input v-model="keyword" @on-change="searchByKeyword" clearable search enter-button placeholder="操作用户或方法名"
+          style="width: 300px" />
+      </FormItem>
+      <FormItem>
+        <Button shape="circle" icon="md-refresh" @click="getTableList"></Button>
+      </FormItem>
+    </Form>
+    <Table border :loading="isloading" highlight-row @on-current-change="getCurrentRow" @on-row-dblclick="getRowClick"
+      :height="tableHeight" :columns="columns" :data="tableList"></Table>
+    <Page :total="total" show-elevator show-sizer show-total :page-size="50" :page-size-opts="[30, 50, 100]" @on-change="getCurrentPage"
+      @on-page-size-change="getPageSize" />
+    <Modal v-model="isShow" title="在办工作详情">
+    </Modal>
   </div>
 </template>
+
 <script>
+import logCollectionApi from "@/api/LogCollection.js";
+
 export default {
-  name: "export-excel",
+  name: "logCollection_page",
   data() {
-    return {};
+    return {
+      columns: [
+        {
+          type: "index",
+          width: 100,
+          align: "center"
+        },
+        {
+          key: "businessAcceptNumber",
+          title: "业务受理号",
+          sortable: true,
+          sortType: "desc",
+          width: 150,
+          align: "center"
+        },
+        {
+          key: "businessName",
+          title: "业务名称",
+          width: 308,
+          align: "center"
+        },
+        {
+          key: "businessStartTime",
+          title: "业务开始时间",
+          width: 150,
+          align: "center"
+        },
+        {
+          key: "taskName",
+          title: "环节名称",
+          width: 300,
+          align: "center"
+        },
+        {
+          key: "taskStartTime",
+          title: "环节开始时间",
+          width: 150,
+          align: "center"
+        },
+        {
+          key: "claimTime",
+          title: "接办时间",
+          width: 150,
+          align: "center"
+        },
+
+        {
+          key: "processDefinitionImage",
+          title: "查看流程状态",
+          width: 150,
+          align: "center"
+        },
+        {
+          key: "businessForm",
+          title: "查看业务表单",
+          align: "center",
+          width: 150
+        }
+      ],
+      isloading: false,
+      tableList: [],
+      total: null,
+      currentPage: 1,
+      pageSize: 50,
+      startDate: null,
+      endDate: null,
+      keyword: null,
+      isShow: false,
+      LogDetail: {},
+      tableHeight: 680
+    };
   },
-  methods: {},
-  created() {},
-  mounted() {}
+  mounted() {
+    this.isloading = true;
+    this.getTableList();
+    // this.tableHeight = window.innerHeight;
+
+    // console.log(
+    //   this.tableHeight,
+    //   window.innerHeight,
+    //   this.$refs.table.$el.offsetTop
+    // );
+  },
+
+  methods: {
+    getTableList: function() {
+      this.isloading = true;
+      logCollectionApi
+        .getOperationLogList(
+          this.currentPage,
+          this.pageSize,
+          this.startDate,
+          this.endDate,
+          this.keyword
+        )
+        .then(res => {
+          this.isloading = false;
+          if (res.status === 0) {
+            this.tableList = res.data.rows;
+            this.total = res.data.total;
+          } else {
+            this.isloading = false;
+            this.$Message.error("else-请求服务器异常");
+          }
+        })
+        .catch(err => {
+          this.isloading = false;
+          this.$Message.error("catch-请求服务器异常");
+        });
+    },
+    getCurrentRow() {
+      //点击某一行时，获取当前行的数据，然后才进行提交接办等操作
+      this.$Message.info("对当前行进行提交或者接办等操作");
+    },
+
+    //双击时，显示详细信息
+    getRowClick(rowData) {
+      this.isShow = true;
+      this.LogDetail = rowData;
+      console.log(rowData);
+    },
+    //搜索框内容变化时，根据关键字立即调用搜索
+    searchByKeyword() {
+      this.getTableList();
+    },
+    //修改页码
+    getCurrentPage(currentPage) {
+      this.currentPage = currentPage;
+      this.getTableList();
+    },
+    //修改每页显示的条数
+    getPageSize(pageSize) {
+      this.pageSize = pageSize;
+      this.getTableList();
+    }
+  }
 };
 </script>
+
+<style scoped>
+.ivu-btn {
+  border-color: #fff;
+}
+</style>
