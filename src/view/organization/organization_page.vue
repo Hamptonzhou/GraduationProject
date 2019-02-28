95fdab90abc1cc6aa6a1146510bef7c89212672d
Hamptonzhou <Hamptonzhou@163.com>
2019-02-28 23:33:56 +0800

    新增人员展示组件

--- src/view/organization/organization_page.vue
+++ src/view/organization/organization_page.vue
@@ -3,18 +3,18 @@
     <div class="split">
       <Split v-model="split" mode="horizontal">
         <div slot="left" class="split-pane">
-          <organization-tree></organization-tree>
+          <organization-tree @childSendParentSelectedNodeId="toShowPeopleComponent">></organization-tree>
         </div>
         <div slot="right" class="split-pane">
-          <Divider type="vertical"/>
+          <Divider type="vertical" />
           <Button type="primary" ghost size="large" @click="changeComponent='addDepartment'">新建部门</Button>
-          <Divider type="vertical"/>
+          <Divider type="vertical" />
           <Button type="primary" ghost size="large" @click="changeComponent='addJobPosition'">新建岗位</Button>
-          <Divider type="vertical"/>
+          <Divider type="vertical" />
           <Button type="primary" ghost size="large" @click="changeComponent='addPeople'">新建人员</Button>
-          <Divider type="vertical"/>
-          <Divider type="horizontal"/>
-          <component :is="changeComponent"></component>
+          <Divider type="vertical" />
+          <Divider type="horizontal" />
+          <component :is="changeComponent" :selectedNodeId="selectedNodeId"></component>
         </div>
       </Split>
     </div>
@@ -26,6 +26,7 @@ import organizationTree from "@/view/organization/organizationTree.vue";
 import addDepartment from "@/view/organization/addDepartment.vue";
 import addJobPosition from "@/view/organization/addJobPosition.vue";
 import addPeople from "@/view/organization/addPeople.vue";
+import showPeople from "@/view/organization/showPeople.vue";
 
 export default {
   name: "organization_page",
@@ -33,13 +34,21 @@ export default {
     organizationTree,
     addDepartment,
     addJobPosition,
-    addPeople
+    addPeople,
+    showPeople
   },
   data() {
     return {
       split: 0.15,
-      changeComponent: "addDepartment"
+      changeComponent: "addDepartment",
+      selectedNodeId: null
     };
+  },
+  methods: {
+    toShowPeopleComponent(selectedNodeId) {
+      this.selectedNodeId = selectedNodeId;
+      this.changeComponent = "showPeople";
+    }
   }
 };
 </script>
