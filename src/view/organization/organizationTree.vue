1dffdd6a8bfed5b8020ba7f9d7761cc8e5dcb187
Hamptonzhou <Hamptonzhou@163.com>
2019-02-28 16:54:08 +0800

    重写组织结构树

--- src/view/organization/organizationTree.vue
+++ src/view/organization/organizationTree.vue
@@ -4,59 +4,55 @@
   </div>
 </template>
 <script>
-import organizationAPI from '@/api/organization.js'
+import organizationAPI from "@/api/organization.js";
 export default {
-  data () {
+  data() {
     return {
       parentId: null,
       newList: [],
       departmentTree: []
-    }
+    };
   },
-  mounted () {
-    this.fetchDepartmentTree()
+  mounted() {
+    this.fetchDepartmentTree();
   },
   methods: {
-    fetchDepartmentTree () {
+    fetchDepartmentTree() {
       organizationAPI
         .getDepartmentTree()
         .then(res => {
           if (res.status === 0) {
-            this.departmentTree = this.getTree(res.data)
+            this.departmentTree = this.getTree(res.data);
           } else {
-            this.$Message.error('else-无法获取部门列表')
+            this.$Message.error("else-无法获取部门列表");
           }
         })
         .catch(err => {
-          this.$Message.error('catch-请求获取部门列表失败')
-        })
+          this.$Message.error("catch-请求获取部门列表失败");
+        });
     },
 
-    getTree (tree = []) {
-      debugger
-      let arr = []
-      if (tree.length !== 0) {
-        tree.forEach(item => {
-          let obj = {}
-          obj.id = item.id
-          obj.title = tree.departmentName || tree.jobPositionName
-          arr.push(obj)
-        })
-      }
-      return arr
+    getTree(tree = []) {
+      let arr = [];
+      let obj = {};
+      obj.id = tree.id;
+      obj.title = tree.title;
+      obj.children = tree.children;
+      obj.expand = true;
+      arr.push(obj);
+      return arr;
     },
-
     // 选中节点
-    selectedNode (node) {
-      this.$Message.info(node)
+    selectedNode(node) {
+      this.$Message.info(node);
     },
 
     // 点击时，根据点击的树节点的parentId去获取当前树节点的所有用户，在右边用卡片显示
-    getCurrentTreeNodePeople () {
+    getCurrentTreeNodePeople() {
       this.$Message.info(
-        '点击时，根据点击的树节点的parentId去获取当前树节点的所有用户，在右边用卡片显示'
-      )
+        "点击时，根据点击的树节点的parentId去获取当前树节点的所有用户，在右边用卡片显示"
+      );
     }
   }
-}
+};
 </script>
