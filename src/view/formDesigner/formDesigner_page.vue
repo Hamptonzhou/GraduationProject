c9965f5b8988a86d181528aa76377c179c927713
hamptonzhou <hamptonzhou@163.com>
2019-03-09 22:35:58 +0800

    为表单渲染组件的添加默认的初始化数据，防止报错size、length未定义

--- src/view/formDesigner/formDesigner_page.vue
+++ src/view/formDesigner/formDesigner_page.vue
@@ -1,9 +1,6 @@
 <template>
   <div style="height: 100%">
-    <!-- <Modal title="流程状态" width="600" v-model="showModal" @on-cancel="showModal=false">
-      <fm-generate-form :data="jsonData" :value="values" ref="generateForm">
-      </fm-generate-form>
-    </Modal> -->
+    <!-- 保存表单模态框 -->
     <Modal v-model="showModal" title="请输入表单名称" @on-ok="handleSave">
       <Form ref="formDesignForm" :model="nameForm" :rules="ruleFormDesign" :label-width="100">
         <FormItem label="表单名称" prop="formName">
@@ -11,13 +8,18 @@
         </FormItem>
       </Form>
     </Modal>
-    <fm-making-form ref="makingForm" preview generate-json generate-code>
+
+    <fm-generate-form :data="jsonData" :value="values" ref="generateForm">
+    </fm-generate-form>
+
+    <fm-making-form ref="makingForm" preview generate-code generate-json>
       <template slot="action">
         <el-button type="text" size="medium" icon="el-icon-download" @click="showModal=true">保存</el-button>
-        <!-- <Select clearable style="float:left;width:200px">
-          <Option value='NewYork'>aaa</Option>
-          <Option value='London'>aaa</Option>
-        </Select> -->
+        <!-- 自定义内容 -->
+        <Select clearable style="float:left;width:200px" v-model="selectedNodeId">
+          <Option v-for="(item,index) in selectList" :key="index" :value="item.id">{{item.formName}}</Option>
+        </Select>
+        <Button type="primary" ghost style="float:left;" @click="viewFormData(selectedNodeId)">编辑表单</Button>
       </template>
     </fm-making-form>
 
@@ -44,6 +46,8 @@ export default {
   name: "formDesigner_page",
   data() {
     return {
+      selectList: null,
+      selectedNodeId: null,
       showModal: false,
       nameForm: {
         formName: ""
@@ -57,23 +61,48 @@ export default {
           }
         ]
       },
-      jsonData: {}, // 表单配置中生成的json数据
+      jsonData: {
+        //表单渲染初始化数据，否则报错list的size、config的length未定义
+        list: [],
+        config: { labelWidth: 100, labelPosition: "right", size: "small" }
+      }, // 表单配置中生成的json数据
       values: {} // 表单需要显示的表单数据
     };
   },
+  mounted() {
+    this.fetchAllFormData();
+  },
   methods: {
+    //获取所有表单数据
+    fetchAllFormData() {
+      formDesignerAPI
+        .getAllFormData()
+        .then(res => {
+          if (res.status === 0) {
+            this.selectList = res.data;
+          } else {
+            this.$Message.error("获取表单数据失败！");
+          }
+        })
+        .catch(err => {
+          this.$Message.error("catch-请求服务器失败");
+        });
+    },
+    //保存表单数据
     handleSave() {
       this.$refs["formDesignForm"].validate(valid => {
         if (valid) {
-          console.log("保存json数据：");
-          let formDataObject = this.$refs.makingForm.getJSON();
-          let formData = JSON.stringify(formDataObject);
-          console.log(this.nameForm.formName);
-          console.log(formData);
+          //直接定义一个对象传递到后台进行映射，但是需要注意，js接口的对象不需要使用{}的形式
+          let BusinessFormData = {
+            id: this.selectedNodeId,
+            formName: this.nameForm.formName,
+            formData: JSON.stringify(this.$refs.makingForm.getJSON()) //json对象转化为json字符串
+          };
           formDesignerAPI
-            .saveFormData(this.nameForm.formName, formData)
+            .saveFormData(BusinessFormData)
             .then(res => {
               if (res.status === 0) {
+                this.fetchAllFormData();
                 this.$Message.success("表单保存成功！");
               } else {
                 this.$Message.error("表单保存失败！");
@@ -86,6 +115,22 @@ export default {
           this.$Message.error("请完善信息!");
         }
       });
+    },
+    //根据表单id从后台获取表单数据进行渲染
+    viewFormData(selectedNodeId) {
+      formDesignerAPI
+        .getFormDataById(selectedNodeId)
+        .then(res => {
+          if (res.status === 0) {
+            let formData = res.data.formData;
+            this.$refs.makingForm.setJSON(JSON.parse(formData));
+          } else {
+            this.$Message.error("无法获取表单信息");
+          }
+        })
+        .catch(err => {
+          this.$Message.error("catch-请求服务器失败");
+        });
     }
   }
 };
