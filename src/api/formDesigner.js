6d0e8788b3c0cfa55c41f484b707a17900b54dac
hamptonzhou <hamptonzhou@163.com>
2019-03-09 22:34:51 +0800

    修改保存表单的接口实现

--- src/api/formDesigner.js
+++ src/api/formDesigner.js
@@ -2,17 +2,18 @@ import config from './config'
 const baseUrl = config.baseUrl
 
 export default {
-  // 保存表单的json数据
-  saveFormData(formName, formData) {
-    return config.doPostPromise(baseUrl + '/FormController/saveFormData', {
-      formName,
-      formData
-    })
+  // 保存表单的json数据，直接定义一个对象传递到后台进行映射，但是需要注意，BusinessFormData对象不需要使用{}括起来
+  saveFormData(BusinessFormData) {
+    return config.doPostPromise(baseUrl + '/FormController/saveOrUpdateFormData', BusinessFormData)
   },
   // 获取表单实体对象
   getFormDataById(formDataId) {
     return config.doGetPromise(baseUrl + '/FormController/getFormDataById', {
       formDataId
     })
+  },
+  // 获取所有表单实体对象
+  getAllFormData() {
+    return config.doGetPromise(baseUrl + '/FormController/getAllFormData')
   },
 }
