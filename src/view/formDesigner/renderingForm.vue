f05ba67dba025dad17399b921db8c0bd9cfe12d7
hamptonzhou <hamptonzhou@163.com>
2019-04-22 00:47:05 +0800

    added：渲染表单数据

--- src/view/formDesigner/renderingForm.vue
+++ src/view/formDesigner/renderingForm.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <Modal v-model="showModal" :title="businessFormName" fullscreen @on-ok="handleSave">
-      <fm-generate-form ref="generateForm" :data="jsonDataObject">
+      <fm-generate-form ref="generateForm" :data="jsonDataObject" :value="formValues">
       </fm-generate-form>
     </Modal>
   </div>
@@ -22,7 +22,9 @@ export default {
           labelPosition: "top",
           size: "small"
         }
-      }
+      },
+      formValues: {},
+      businessFormId: null
     };
   },
   //   watch: {
@@ -32,13 +34,20 @@ export default {
   //     }
   //   },
   methods: {
+    //渲染表单模板
     showForm(businessFormId, businessFormNaming) {
+      this.businessFormId = businessFormId;
       formDesignerAPI
         .getFormDataById(businessFormId)
         .then(res => {
+          console.log(res.data);
           if (res.status === 0) {
             this.businessFormName = businessFormNaming || "业务表单";
+            //渲染表单模板数据
             this.jsonDataObject = JSON.parse(res.data.formData);
+            //渲染表单数据
+            console.log(res.data.formDataValues);
+            this.formValues = JSON.parse(res.data.formDataValues);
             this.showModal = true;
           } else {
             this.$Message.error("无法获取业务表单！");
@@ -48,8 +57,35 @@ export default {
           this.$Message.error("catch-请求服务器异常");
         });
     },
+    //保存表单数据
     handleSave() {
-      this.$Message.info("把填写好的信息存库");
+      //获取表单的值
+      this.$refs.generateForm
+        .getData()
+        .then(data => {
+          console.log(JSON.stringify(data));
+          //表单对象
+          let BusinessFormData = {
+            id: this.businessFormId,
+            formDataValues: JSON.stringify(data) //json对象转化为json字符串
+          };
+          //保存
+          formDesignerAPI
+            .saveFormData(BusinessFormData)
+            .then(res => {
+              if (res.status === 0) {
+                this.$Message.success("保存业务表单数据成功！");
+              } else {
+                this.$Message.error("保存业务表单数据失败！");
+              }
+            })
+            .catch(err => {
+              this.$Message.error("请求服务器失败");
+            });
+        })
+        .catch(e => {
+          this.$Message.error("获取表单数据失败！");
+        });
     }
   }
 };
