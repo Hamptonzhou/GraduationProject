5cfde11ba100a4890807e2cde1518adae1ef4f2d
hamptonzhou <hamptonzhou@163.com>
2019-04-23 02:10:28 +0800

    added:完成卡片数量的展示

--- src/view/home/home.vue
+++ src/view/home/home.vue
@@ -1,40 +1,48 @@
 <template>
   <div>
     <Row :gutter="20">
+      <!-- 卡片 -->
       <i-col :xs="12" :md="8" :lg="4" v-for="(infor, i) in inforCardData" :key="`infor-${i}`" style="height: 120px;padding-bottom: 10px;">
         <infor-card shadow :color="infor.color" :icon="infor.icon" :icon-size="36">
-          <count-to :end="infor.count" count-class="count-style"/>
+          <count-to :end="infor.count" count-class="count-style" />
           <p>{{ infor.title }}</p>
         </infor-card>
       </i-col>
     </Row>
     <Row :gutter="20" style="margin-top: 10px;">
+      <!-- 饼状图 -->
       <i-col :md="24" :lg="8" style="margin-bottom: 20px;">
         <Card shadow>
           <chart-pie style="height: 300px;" :value="pieData" text="用户访问来源"></chart-pie>
         </Card>
       </i-col>
+      <!-- 柱状图 -->
       <i-col :md="24" :lg="16" style="margin-bottom: 20px;">
         <Card shadow>
-          <chart-bar style="height: 300px;" :value="barData" text="每周用户活跃量"/>
+          <chart-bar style="height: 300px;" :value="barData" text="每周用户活跃量" />
         </Card>
       </i-col>
     </Row>
+    <!-- 折线图 -->
     <Row>
       <Card shadow>
-        <example style="height: 310px;"/>
+        <example style="height: 310px;" />
       </Card>
     </Row>
   </div>
 </template>
 
 <script>
-import InforCard from '_c/info-card'
-import CountTo from '_c/count-to'
-import { ChartPie, ChartBar } from '_c/charts'
-import Example from './example.vue'
+import InforCard from "_c/info-card";
+import CountTo from "_c/count-to";
+import { ChartPie, ChartBar } from "_c/charts";
+import Example from "./example.vue";
+
+import { mapState } from "vuex";
+import workflowDesignApi from "@/api/workflowDesign.js";
+import formDesignerAPI from "@/api/formDesigner.js";
 export default {
-  name: 'home',
+  name: "home",
   components: {
     InforCard,
     CountTo,
@@ -42,22 +50,42 @@ export default {
     ChartBar,
     Example
   },
-  data () {
+  data() {
     return {
       inforCardData: [
-        { title: '新增用户', icon: 'md-person-add', count: 803, color: '#2d8cf0' },
-        { title: '累计点击', icon: 'md-locate', count: 232, color: '#19be6b' },
-        { title: '新增问答', icon: 'md-help-circle', count: 142, color: '#ff9900' },
-        { title: '分享统计', icon: 'md-share', count: 657, color: '#ed3f14' },
-        { title: '新增互动', icon: 'md-chatbubbles', count: 12, color: '#E46CBB' },
-        { title: '新增页面', icon: 'md-map', count: 14, color: '#9A66E4' }
+        {
+          title: "业务定义",
+          icon: "md-create",
+          count: 0,
+          color: "#2d8cf0"
+        },
+        { title: "正在办理", icon: "ios-hand", count: 0, color: "#ed3f14" },
+        {
+          title: "个人已办理",
+          icon: "ios-person",
+          count: 0,
+          color: "#ff9900"
+        },
+        {
+          title: "办理完结业务",
+          icon: "md-checkbox",
+          count: 0,
+          color: "#19be6b"
+        },
+        {
+          title: "可用流程",
+          icon: "md-build",
+          count: 0,
+          color: "#E46CBB"
+        },
+        { title: "可用表单", icon: "md-grid", count: 0, color: "#9A66E4" }
       ],
       pieData: [
-        {value: 335, name: '直接访问'},
-        {value: 310, name: '邮件营销'},
-        {value: 234, name: '联盟广告'},
-        {value: 135, name: '视频广告'},
-        {value: 1548, name: '搜索引擎'}
+        { value: 335, name: "直接访问" },
+        { value: 310, name: "邮件营销" },
+        { value: 234, name: "联盟广告" },
+        { value: 135, name: "视频广告" },
+        { value: 1548, name: "搜索引擎" }
       ],
       barData: {
         Mon: 13253,
@@ -68,16 +96,120 @@ export default {
         Sat: 1322,
         Sun: 1324
       }
-    }
+    };
   },
-  mounted () {
-    //
+  computed: {
+    ...mapState({
+      userName: state => state.user.userName,
+      userId: state => state.user.userId
+    })
+  },
+  created() {
+    this.getBusinessDefinitionCount();
+    this.getHanglingWorkCount();
+    this.getPersonalDoneWorkCount();
+    this.getFinishedWorkCount();
+    this.getUseableProcessCount();
+    this.getUseableFormCount();
+  },
+  mounted() {},
+  methods: {
+    //获取业务定义数量
+    getBusinessDefinitionCount: function() {
+      workflowDesignApi
+        .getBusinessDefinitionList(1, 100, "", this.userId)
+        .then(res => {
+          if (res.status === 0) {
+            this.inforCardData[0].count = res.data.total;
+          } else {
+            this.$Message.error("获取业务定义数量失败");
+          }
+        })
+        .catch(err => {
+          this.$Message.error("请求服务器异常");
+        });
+    },
+    //获取正在办理数量
+    getHanglingWorkCount: function() {
+      workflowDesignApi
+        .getHanglingWorkList(1, 100, "", this.userName)
+        .then(res => {
+          if (res.status === 0) {
+            this.inforCardData[1].count = res.data.total;
+          } else {
+            this.$Message.error("获取正在办理数量失败");
+          }
+        })
+        .catch(err => {
+          this.$Message.error("请求服务器异常");
+        });
+    },
+    //获取个人已办理数量
+    getPersonalDoneWorkCount: function() {
+      workflowDesignApi
+        .getPersonalDoneWorkList(1, 100, "", this.userName)
+        .then(res => {
+          if (res.status === 0) {
+            this.inforCardData[2].count = res.data.total;
+          } else {
+            this.$Message.error("获取个人已办理数量失败");
+          }
+        })
+        .catch(err => {
+          this.$Message.error("请求服务器异常");
+        });
+    },
+    //获取办结工作数量
+    getFinishedWorkCount: function() {
+      workflowDesignApi
+        .getFinishedWorkList(1, 100, "", this.userName)
+        .then(res => {
+          if (res.status === 0) {
+            this.inforCardData[3].count = res.data.total;
+          } else {
+            this.$Message.error("获取办结工作数量失败");
+          }
+        })
+        .catch(err => {
+          this.$Message.error("请求服务器异常");
+        });
+    },
+    //获取可用流程数量
+    getUseableProcessCount: function() {
+      workflowDesignApi
+        .getUseableProcessCount()
+        .then(res => {
+          if (res.status === 0) {
+            this.inforCardData[4].count = res.data;
+          } else {
+            this.$Message.error("获取可用流程数量失败");
+          }
+        })
+        .catch(err => {
+          this.$Message.error("请求服务器异常");
+        });
+    },
+    //获取可用表单数量
+    getUseableFormCount() {
+      formDesignerAPI
+        .getAllFormData()
+        .then(res => {
+          if (res.status === 0) {
+            this.inforCardData[5].count = res.data.length;
+          } else {
+            this.$Message.error("获取可用表单数量失败");
+          }
+        })
+        .catch(err => {
+          this.$Message.error("请求服务器异常");
+        });
+    }
   }
-}
+};
 </script>
 
 <style lang="less">
-.count-style{
+.count-style {
   font-size: 50px;
 }
 </style>
